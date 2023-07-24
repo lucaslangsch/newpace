6be312ece6904f999e61b2d3d581d0183f27dd5f
lucaslangsch <lucas.langsch@gmail.com>
2023-07-24 15:33:25 -0300

    fix: configs google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import { Route, Switch } from 'react-router-dom';
+import { useEffect } from 'react';
 import './App.css';
 import Home from './pages/Home';
 import Plans from './pages/Plans';
 import Locality from './pages/Locality';
+import ReactGA from 'react-ga';
+import { useLocation } from 'react-router-dom/cjs/react-router-dom.min';
+
+const TRACKING_ID = "G-C2E59CF5GX";
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
+ const location = useLocation().pathname;
+
+  useEffect(() => {
+    ReactGA.pageview(location);
+  }, []);
+  
   return (
     <Switch>
       <Route exact path="/planos" component={ Plans } />
