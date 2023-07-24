4dfa3e08a5caf022cad2dbab2fcb7a0fecc3fb04
lucaslangsch <lucas.langsch@gmail.com>
2023-07-24 09:00:30 -0300

    fix: use function component in Map and hooks for unMount

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,48 +1,51 @@
-import React, { Component } from 'react';
-import { GoogleMap, LoadScript, MarkerF } from '@react-google-maps/api';
+import React, { useCallback, useEffect, useRef } from 'react';
+import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api';
 
 const containerStyle = {
   width: '100%',
   height: '100%'
 };
+
 const key = import.meta.env.VITE_API_GKEY   // Usa a chave de produção durante a fase de produção
-console.log(import.meta.env);
 const options = {
   streetViewControl: false,
   mapTypeControl: false,
-}
+};
 
-class Map extends Component {
-  render() {
-    const {location} = this.props;
-    return (
-      <>
-        {
-          window.google === undefined ? 
-            <LoadScript
-              googleMapsApiKey={key}
-            >
-            <GoogleMap
-              mapContainerStyle={containerStyle}
-              center={location}
-              zoom={14}
-              options={options}
-            >
-              <MarkerF position={location}/>
-            </GoogleMap>
-            </LoadScript> :
-            <GoogleMap
-              mapContainerStyle={containerStyle}
-              center={location}
-              zoom={14}
-              options={options}
-            >
-              <MarkerF position={location}/>
-            </GoogleMap>
-        }
-      </>
-    )
-  }
-}
+const Map = ({ location }) => {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: key
+  });
+
+  const mapRef = useRef(null);
+
+  const onLoad = useCallback(function callback(map) {
+    mapRef.current = map;
+  }, []);
+
+  const onUnmount = useCallback(function callback() {
+  }, []);
+
+  useEffect(() => {
+    return () => {
+      onUnmount();
+    };
+  }, [onUnmount]);
+
+  if (!isLoaded) return null;
+
+  return (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={location}
+      zoom={14}
+      options={options}
+      onLoad={onLoad}
+    >
+      <Marker position={location} />
+    </GoogleMap>
+  );
+};
 
 export default Map;
